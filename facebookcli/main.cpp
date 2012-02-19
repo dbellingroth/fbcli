415ce1c22392d2af405e6ab6fee7bc1009efe8c9
David Bellingroth <dbellingroth@gmail.com>
2012-02-19 15:36:33 +0100

    Initial Commit

---
+++ facebookcli/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  facebookcli
+//
+//  Created by David Bellingroth on 19.02.12.
+//  Copyright (c) 2012 Burbachstraße 5. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
